d1e5c731ec6a8ae52dc9dc041495f1d4430073d9
SLin24 <105018276+SLin24@users.noreply.github.com>
2022-06-21 15:49:57 -0700

    Update Bellman-Ford.cpp

--- Graph Algorithms/Bellman-Ford.cpp
+++ Graph Algorithms/Bellman-Ford.cpp
@@ -3,77 +3,73 @@
 using namespace std;
 typedef long long ll;
 ll MOD = 1000000007;
-
+//Bellman Ford's Algorithm except with finding maximum path length and detecting positive cycles. (CSES High Score)
 struct Edge {
     ll a, b, x;
 };
 int n;
-bool pass = false;
 bool visited[2500];
-//CSES High Score
-//Bellman Ford's Algorithm, except used to find maximum distance with "positive" cycles instead of negative cycles (O(N*M) algorithm)
-//only reason there is a dfs here is to check if the "positive" cycle is connected to both the starting point and the ending point. 
-void dfs(const int& index, const int& target, const vector<int> *adj) {
-    if (index == target || pass) {
-        pass = true;
-        return;
-    }
+bool bVisited[2500];
+void dfs(const int& index, const vector<int> *adj) {
     for (int x: adj[index]) {
         if (!visited[x]) {
             visited[x] = true;
-            dfs(x, target, adj);
+            dfs(x, adj);
         }
     }
 }
-
+ 
+void bDfs(const int& index, const vector<int> *adj) {
+    for (int x: adj[index]) {
+        if (!bVisited[x]) {
+            bVisited[x] = true;
+            bDfs(x, adj);
+        }
+    }
+}
+ 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
+ 
     int m; cin >> n >> m;
-
+ 
     ll dist[n];
-
+ 
     for (int i = 0; i < n; i++) {
         dist[i] = -1e18;
     }
     Edge edges[m];
     vector<int> adj[n];
+    vector<int> adj2[n];
     for (int i = 0; i < m; i++) {
         ll a, b, x; cin >> a >> b >> x;
         a--, b--;
         adj[a].push_back(b);
+        adj2[b].push_back(a);
         edges[i] = {a, b, x};
     }
-    //initialize starting node distance to 0, and iterate through all edges
+    dfs(0, adj);
+    bDfs(n - 1, adj2);
     dist[0] = 0;
+    bool ok = true;
+    //Iterate over all m edges n times. Time complexity will be O(N * M)
     for (int i = 0; i < n; i++) {
         for (auto e: edges) {
             dist[e.b] = max(dist[e.b], (dist[e.a] + e.x));
         }
     }
-    ll distCheck[n]{};
+    ll distCheck[n];
     for (int i = 0; i < n; i++) {
         distCheck[i] = -1e18;
     }
     for (auto e: edges) {
-        distCheck[e.b] = max(distCheck[e.b], max(dist[e.b], (dist[e.a] + e.x)));
+        distCheck[e.b] = max(distCheck[e.b], max(dist[e.b], dist[e.a] + e.x));
     }
-    bool ok = true;
+    //checks if the "positive" cycle is within the path between the start node and the finish node
     for (int i = 0; i < n; i++) {
-        if (distCheck[i] > dist[i]) {
-            pass = false;
-            //wastes a lot of time with memset (There's a more efficient way to check if a positive cycle is connected to the start and end nodes)
-            memset(visited, false, sizeof(visited));
-            dfs(i, n - 1, adj);
-            if (!pass) continue;
-            pass = false;
-            memset(visited, false, sizeof(visited));
-            dfs(0, i, adj);
-            if (pass) {
-                ok = false;
-                break;
-            }
+        if (distCheck[i] > dist[i] && visited[i] && bVisited[i]) {
+            ok = false;
         }
     }
     cout << (ok?dist[n - 1]:-1) << '\n';
